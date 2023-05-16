f47622dd2f81efd60f17dc44e15467babefd1894
Kumanan <kumanan1731@gmail.com>
2023-05-17 06:31:07 +0800

    switch case

--- 8 kyu  - Return the day/main.js
+++ 8 kyu  - Return the day/main.js
@@ -12,13 +12,23 @@ Complete the function which returns the weekday according to the input number:
 Otherwise returns "Wrong, please enter a number between 1 and 7"
 */
 
-whatday = (num) =>
-	[
-		'Sunday',
-		'Monday',
-		'Tuesday',
-		'Wednesday',
-		'Thursday',
-		'Friday',
-		'Saturday',
-	][num - 1] || 'Wrong, please enter a number between 1 and 7';
+function whatday(num) {
+	switch (num) {
+		case 1:
+			return 'Sunday';
+		case 2:
+			return 'Monday';
+		case 3:
+			return 'Tuesday';
+		case 4:
+			return 'Wednesday';
+		case 5:
+			return 'Thursday';
+		case 6:
+			return 'Friday';
+		case 7:
+			return 'Saturday';
+		default:
+			return 'Wrong, please enter a number between 1 and 7';
+	}
+}
